8f55dc386b7add9b411a611703de898ba459d523
Thomas <tpasquier.pro@gmail.com>
2026-08-22 14:40:01 +0200

    save file with new env variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,23 +1,19 @@
 import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
-config({
-  path: process.env.ENV_FILE || '.env.local',
-});
+config({ path: '.env.preprod' });
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL manquant');
+  throw new Error('DATABASE_URL manquant — vérifie ton fichier .env.local');
 }
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
-
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
-
   strict: true,
   verbose: true,
 });
